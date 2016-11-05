f021949b77c73464f7185139678097211283914d
Nick0603 <b10303008@gmail.com>
2016-11-06 04:59:11 +0800

    Update: 1.程式可重複執行 按下Enter鍵後重新載入資料，2.打包產出exe

--- stock.py
+++ stock.py
@@ -31,28 +31,34 @@ def floatIndex(my_list,f):
 Ammount = 12;
 
 ids = [0,212876,2,211022,4,211135,212882,7,212459,211283,211024,211044,211020]
-Ratioes = [0]
-for i in range(1,Ammount):
-    id = ids[i]
-    if(id != i):
-        Ratioes.append(float(getAccountRatio(id)))
-    else:
-        Ratioes.append(-1000)
+
+while(1):
+    Ratioes = [0]
+    for i in range(1,Ammount):
+        id = ids[i]
+        if(id != i):
+            Ratioes.append(float(getAccountRatio(id)))
+        else:
+            Ratioes.append(-1000)
 
 
-sort_Ratioes = Ratioes[:]
-sort_Ratioes.sort(reverse = True)
+    sort_Ratioes = Ratioes[:]
+    sort_Ratioes.sort(reverse = True)
 
-for i in range(1,Ammount):
-    Ratio = sort_Ratioes[i]
-    index = floatIndex(Ratioes,sort_Ratioes[i])
-    
-    if Ratio != -1000:      
-        result = "目前第%2d名 組別：%2d 總報酬率：%5.3f%%" %(i,index,Ratio)
-        print(result)
-    
-for i in range(1,Ammount):
-    if Ratioes[i] == -1000:
-        result = "目前組別：%2d 無法讀取到帳戶資料"%i 
-        print(result)
+    for i in range(1,Ammount):
+        Ratio = sort_Ratioes[i]
+        index = floatIndex(Ratioes,sort_Ratioes[i])
+        
+        if Ratio != -1000:      
+            result = "目前第%2d名 組別：%2d 總報酬率：%5.3f%%" %(i,index,Ratio)
+            print(result)
+            
+    print("----------------------------------------")
+    for i in range(1,Ammount):
+        if Ratioes[i] == -1000:
+            result = "目前組別：%2d 無法讀取到帳戶資料"%i 
+            print(result)
+
+    print("----------------------------------------")
+    mode = input('按下Enter鍵重新載入資料')
 
