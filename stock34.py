7c1c9d5d463d554d8d7de5fdefe1f0e16e117db6
Nick0603 <b10303008@gmail.com>
2016-11-07 22:25:08 +0800

    Update:修補 list中index錯誤(range1~12 缺12)

--- stock34.py
+++ stock34.py
@@ -1,6 +1,7 @@
 #encoding:UTF-8
 import urllib.request
 import json
+import datetime
 
 def getAccountInfo(id):
     url = "http://www.cmoney.tw/vt/ashx/accountdata.ashx?act=AccountInfo&aid=" + str(id)
@@ -30,35 +31,40 @@ def floatIndex(my_list,f):
 
 Ammount = 12;
 
-ids = [0,212876,2,211022,4,211135,212882,7,212459,211283,211024,211044,211020]
+ids = [212876,211616,211022,4,211135,212882,7,212459,211283,211024,211044,211020]
 
 while(1):
-    Ratioes = [0]
-    for i in range(1,Ammount):
+    
+    Ratioes = []
+    for i in range(0,Ammount):
         id = ids[i]
-        if(id != i):
+        if(id != i+1):
             Ratioes.append(float(getAccountRatio(id)))
         else:
             Ratioes.append(-1000)
 
-
     sort_Ratioes = Ratioes[:]
     sort_Ratioes.sort(reverse = True)
 
-    for i in range(1,Ammount):
+    print("----------------------------------------")
+    nowTime = datetime.datetime.now();
+    Date_text = nowTime.strftime('%y/%m/%d')
+    Time_text = nowTime.strftime('%H:%M:%S')
+    print("今天日期：{date} 現在時間：{time}".format(date=Date_text,time=Time_text))
+    
+    for i in range(0,Ammount):
         Ratio = sort_Ratioes[i]
         index = floatIndex(Ratioes,sort_Ratioes[i])
         
         if Ratio != -1000:      
-            result = "目前第%2d名 組別：%2d 總報酬率：%5.3f%%" %(i,index,Ratio)
+            result = "目前第%2d名 組別：%2d 總報酬率：%5.3f%%" %(i+1,index+1,Ratio)
             print(result)
-            
+
     print("----------------------------------------")
-    for i in range(1,Ammount):
+    for i in range(0,Ammount):
         if Ratioes[i] == -1000:
-            result = "目前組別：%2d 無法讀取到帳戶資料"%i 
+            result = "目前組別：%2d 無法讀取到帳戶資料"%(i+1)
             print(result)
 
     print("----------------------------------------")
     mode = input('按下Enter鍵重新載入資料')
-
